296226b678dd810d499eee679d742e1997a7594d
romualdas <romualdas.gircys@interstartas.lt>
2024-03-20 21:39:07 +0200

    draw board and pieces

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
 
-        board.draw_sqaures(WIN)
+        board.draw(WIN)
         pygame.display.update()
     
     pygame.quit()
